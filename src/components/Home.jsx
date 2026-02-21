3175f82819367a13e4a7c08abffd04070c52afa5
XP_STARTS <abdulmoid47628@gmail.com>
2026-02-21 07:52:29 +0500

    Just added the miniNavbar Links so it can navigate

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,17 +12,17 @@ import QuickBitesCards from '../components/homeCards/QuickBitesCards'
 const Home = () => {
   return (
     <div className='min-w-screen w-full'>
-        <Corousel />
-        <MiniNavbar />
+      <Corousel />
+      <MiniNavbar />
 
-    <div className='w-full h-auto bg-black/10 flex flex-col items-center justify-center'>
-      <DealsCards />
-      <ChickenBurgerCards />
-      <BeefBurgerCards />
-      <PizzaCards />
-      <FriesCards />
-      <QuickBitesCards />
-    </div>
+      <div className='w-full h-auto bg-black/10 flex flex-col items-center justify-center'>
+        <div id="deals"><DealsCards /></div>
+        <div id="chicken"><ChickenBurgerCards /></div>
+        <div id="beef"><BeefBurgerCards /></div>
+        <div id="pizza"><PizzaCards /></div>
+        <div id="fries"><FriesCards /></div>
+        <div id="quick"><QuickBitesCards /></div>
+      </div>
 
     </div>
   )
--- src/components/HomeComponents/MiniNavbar.jsx
+++ src/components/HomeComponents/MiniNavbar.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const MiniNavbar = () => {
+
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id)
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" })
+    }
+  }
+
   return (
-    <div className='h-15 flex gap-10 justify-center text-2xl font-bold bg-gray-600/20 items-center '>
-        
-        <Link className='hover:text-orange-400 hover:border-b-orange-400 hover:border-b-2'>Deals</Link>
-        <Link className='hover:text-orange-400 hover:border-b-orange-400 hover:border-b-2'>Chicken Burgers</Link>
-        <Link className='hover:text-orange-400 hover:border-b-orange-400 hover:border-b-2'>Beef Burgers</Link>
-        <Link className='hover:text-orange-400 hover:border-b-orange-400 hover:border-b-2'>Pizza</Link>
-        <Link className='hover:text-orange-400 hover:border-b-orange-400 hover:border-b-2'>Quick Bites</Link>
-        <Link className='hover:text-orange-400 hover:border-b-orange-400 hover:border-b-2'>Fries</Link>
+    <div className='h-15 flex gap-10 justify-center text-2xl font-bold bg-gray-600/20 items-center'>
+
+      <button onClick={() => scrollToSection("deals")} className="hover:text-orange-400 hover:border-b-2 hover:border-b-orange-400">Deals</button>
+
+      <button onClick={() => scrollToSection("chicken")} className="hover:text-orange-400 hover:border-b-2 hover:border-b-orange-400">Chicken Burgers</button>
+
+      <button onClick={() => scrollToSection("beef")} className="hover:text-orange-400 hover:border-b-2 hover:border-b-orange-400">Beef Burgers</button>
+
+      <button onClick={() => scrollToSection("pizza")} className="hover:text-orange-400 hover:border-b-2 hover:border-b-orange-400">Pizza</button>
+
+      <button onClick={() => scrollToSection("quick")} className="hover:text-orange-400 hover:border-b-2 hover:border-b-orange-400">Quick Bites</button>
+
+      <button onClick={() => scrollToSection("fries")} className="hover:text-orange-400 hover:border-b-2 hover:border-b-orange-400">Fries</button>
 
     </div>
   )
